b548d91d7afe31d7daa007dc5e5a92d1caca8f82
achdaisy <oduordaizy@gmail.com>
2026-01-29 02:26:12 +0300

    solving migration issues

--- backend/accounts/migrations/0008_alter_user_phone_number_alter_user_profile_picture.py
+++ backend/accounts/migrations/0008_alter_user_phone_number_alter_user_profile_picture.py
@@ -19,11 +19,6 @@ class Migration(migrations.Migration):
         migrations.RunPython(delete_null_phones),
         migrations.AlterField(
             model_name='user',
-            name='phone_number',
-            field=models.CharField(blank=True, max_length=15, null=True, unique=True, validators=[django.core.validators.RegexValidator(message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.", regex='^\\+?1?\\d{9,15}$')]),
-        ),
-        migrations.AlterField(
-            model_name='user',
             name='profile_picture',
             field=models.ImageField(blank=True, default='profile_pictures/default-profile.png', null=True, upload_to='profile_pictures/'),
         ),
