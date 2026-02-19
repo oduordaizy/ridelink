6545bbaab1abcaa251b61b8da1cfd5ed0b5cc93f
achdaisy <oduordaizy@gmail.com>
2026-02-19 03:37:18 +0300

    fixed the already registered error

--- backend/rides/admin.py
+++ backend/rides/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from .models import Ride, Booking, RideImage
-from payments.models import Wallet, Transaction
 
 # Register your models here.
 admin.site.register(Ride)
 admin.site.register(RideImage)
 admin.site.register(Booking)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
